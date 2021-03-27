260d434b0718342786a03787084d3400d93fa7a7
RahulThirumala <thirumala.rahul@gmail.com>
2021-03-27 03:46:30 +0000

    done

--- Program-8.c
+++ Program-8.c
@@ -6,6 +6,7 @@ int main()
     int x, y;
     printf(" Enter the value of x: ");
     scanf("%d",&x);
+  
     printf("y = %d  ",y=++x);
     printf("y = %d  ",y=x++);
     printf("y = %d  ",y=--x);
